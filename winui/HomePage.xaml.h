dc4abae6e90ff3ccb6f68beb67a1b84bf813a1c2
lmg325586 <lmg325586@163.com>
2026-08-08 11:47:58 +0800

    为主页展示框添加了流动光效

--- winui/HomePage.xaml.h
+++ winui/HomePage.xaml.h
@@ -7,6 +7,15 @@ namespace winrt::winui::implementation
     struct HomePage : HomePageT<HomePage>
     {
         HomePage();
+
+        void GlowPointerEntered(winrt::Windows::Foundation::IInspectable const& sender,
+            winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& args);
+
+        void GlowPointerMoved(winrt::Windows::Foundation::IInspectable const& sender,
+            winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& args);
+
+        void GlowPointerExited(winrt::Windows::Foundation::IInspectable const& sender,
+            winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& args);
     };
 }
 
