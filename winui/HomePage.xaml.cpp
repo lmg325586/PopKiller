2638a56a806f6caaf21418058438af9a7ece29ed
lmg325586 <lmg325586@163.com>
2026-08-08 10:39:22 +0800

    主页添加了展示系统版本

--- winui/HomePage.xaml.cpp
+++ winui/HomePage.xaml.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "HomePage.xaml.h"
+#include <winrt/Windows.System.Profile.h>
 #if __has_include("HomePage.g.cpp")
 #include "HomePage.g.cpp"
 #endif
@@ -12,13 +13,24 @@ using namespace Microsoft::UI::Xaml;
 
 namespace winrt::winui::implementation
 {
-    int32_t HomePage::MyProperty()
+
+    HomePage::HomePage()
     {
-        throw hresult_not_implemented();
+        InitializeComponent();
+
+        uint64_t v = std::stoull(winrt::to_string(
+            winrt::Windows::System::Profile::AnalyticsInfo::VersionInfo().DeviceFamilyVersion()));
+
+        uint16_t major = static_cast<uint16_t>((v & 0xFFFF000000000000) >> 48);
+        uint16_t minor = static_cast<uint16_t>((v & 0x0000FFFF00000000) >> 32);
+        uint16_t build = static_cast<uint16_t>((v & 0x00000000FFFF0000) >> 16);
+        uint16_t rev = static_cast<uint16_t>(v & 0x000000000000FFFF);
+
+        hstring name = (build >= 22000) ? L"Windows 11" : L"Windows 10";
+
+        WindowsVersionText().Text(
+            name + L"  " + to_hstring(major) + L"." + to_hstring(minor) +
+            L"  Build " + to_hstring(build) + L"." + to_hstring(rev));
     }
 
-    void HomePage::MyProperty(int32_t /* value */)
-    {
-        throw hresult_not_implemented();
-    }
 }
